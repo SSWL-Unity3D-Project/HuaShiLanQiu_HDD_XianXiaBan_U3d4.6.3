f8aad443704936d358db44520655939a5e6f9f16
syq <shenyongqiang2008@126.com>
2018-04-25 10:39:55 +0800

    初步添加连发篮球的逻辑

--- Client/SSBallSpawnPoint.cs
+++ Client/SSBallSpawnPoint.cs
@@ -6,20 +6,84 @@ public class SSBallSpawnPoint : MonoBehaviour
     /// 篮球预制.
     /// </summary>
     public GameObject[] m_BallPrefabArray;
+    /// <summary>
+    /// 产生篮球的间隔时间控制.
+    /// </summary>
+    [Range(0.03f, 10f)]
     public float m_TimeMinSpawn = 2f;
-    float m_LastBallSpawn = 0f;
-	// Update is called once per frame
-	void Update()
+    float m_TimeBallSpawnRecord = 0f;
+    /// <summary>
+    /// 连发球间隔最短时间控制.
+    /// </summary>
+    [Range(0.03f, 10f)]
+    public float m_TimeMinLianFa = 0.3f;
+    public int m_MaxLianFaBallNum = 5;
+    public int m_MinLianFaBallNum = 2;
+    int m_LianFaBallNum = 3;
+    int m_LianFaBallCount = 0;
+    /// <summary>
+    /// 连发球产生点索引.
+    /// </summary>
+    int m_IndexLianFaSpawn = 0;
+    /// <summary>
+    /// 是否连发篮球.
+    /// </summary>
+    bool IsLianFaBall = false;
+    SSTimeDownCtrl m_TimeDownCom;
+
+    float m_TimeLastBallSpawn = 0f;
+    void Start()
     {
-        if (Time.time - m_LastBallSpawn >= m_TimeMinSpawn)
+        m_TimeBallSpawnRecord = m_TimeMinSpawn;
+        m_TimeDownCom = gameObject.AddComponent<SSTimeDownCtrl>();
+        m_TimeDownCom.Init(3600f, 3f);
+        m_TimeDownCom.OnTimeDownStepEvent += OnTimeDownStepLianFaBallEvent;
+    }
+
+    /// <summary>
+    /// 连发球时间到达事件响应.
+    /// </summary>
+    void OnTimeDownStepLianFaBallEvent(float timeVal)
+    {
+        if (!IsLianFaBall)
         {
-            m_LastBallSpawn = Time.time;
-            SpawnGameBall();
+            if (Random.Range(0, 100) % 2 == 0)
+            {
+                IsLianFaBall = true;
+                m_LianFaBallCount = 0;
+                m_TimeMinSpawn = m_TimeMinLianFa;
+                m_LianFaBallNum = (Random.Range(0, 100) % (m_MaxLianFaBallNum - m_MinLianFaBallNum + 1)) + m_MinLianFaBallNum;
+                m_IndexLianFaSpawn = Random.Range(0, 1000) % m_SpawnPointTrArray.Length;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - m_TimeLastBallSpawn >= m_TimeMinSpawn)
+        {
+            m_TimeLastBallSpawn = Time.time;
+            if (IsLianFaBall)
+            {
+                //连续发射篮球.
+                SpawnGameBall(m_IndexLianFaSpawn);
+                m_LianFaBallCount++;
+                if (m_LianFaBallCount >= m_LianFaBallNum)
+                {
+                    IsLianFaBall = false;
+                    m_TimeMinSpawn = m_TimeBallSpawnRecord;
+                }
+            }
+            else
+            {
+                SpawnGameBall();
+            }
         }
 	}
     
     public Transform[] m_SpawnPointTrArray;
-    void SpawnGameBall()
+    void SpawnGameBall(int indexBallSpawn = -1)
     {
         if (m_BallPrefabArray.Length <= 0)
         {
@@ -39,7 +103,7 @@ public class SSBallSpawnPoint : MonoBehaviour
             return;
         }
 
-        int randVal = Random.Range(0, 100) % m_SpawnPointTrArray.Length;
+        int randVal = (indexBallSpawn == -1) ? Random.Range(0, 100) % m_SpawnPointTrArray.Length : indexBallSpawn;
         if (m_SpawnPointTrArray[randVal] == null)
         {
             return;
---
+++ Client/TimeManage/SSTimeUpCtrl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class SSTimeUpCtrl : MonoBehaviour
+{
+    float MaxTimeVal = 0f;
+    float LastTimeVal = 0f;
+    bool IsInitUpTime = false;
+    bool IsTimeUpOver = false;
+    /// <summary>
+    /// 时间上升结束事件.
+    /// </summary>
+    public delegate void TimeUpOverEvent();
+    public event TimeUpOverEvent OnTimeUpOverEvent;
+    void OnTimeUpOver()
+    {
+        if (OnTimeUpOverEvent != null)
+        {
+            OnTimeUpOverEvent();
+        }
+    }
+
+    /// <summary>
+    /// 初始化.
+    /// </summary>
+    public void Init(float maxTime)
+    {
+        MaxTimeVal = maxTime;
+        LastTimeVal = Time.realtimeSinceStartup;
+        IsInitUpTime = true;
+    }
+    
+    void Update()
+    {
+        if (!IsInitUpTime)
+        {
+            return;
+        }
+        
+        if (Time.realtimeSinceStartup - LastTimeVal >= MaxTimeVal)
+        {
+            if (!IsTimeUpOver)
+            {
+                IsTimeUpOver = true;
+                OnTimeUpOver();
+                Destroy(this);
+                return;
+            }
+        }
+    }
+}
